de5ae78fc85482c79d3b3f41ccfee1a8cca4f66f
rkfr <e.xbyvnqn@gmail.com>
2019-03-06 15:34:10 +0200

    added basic slider markup

--- js/room-slider.js
+++ js/room-slider.js
@@ -5,28 +5,71 @@ const dataToShow = [
 		title: 'Капитальный ремонт',
 		location: 'Одинцово',
 		square: '16 м',
-		image: {
+		images: {
 			smallImage: {
-				alt: '',
-				url: ''
+				alt: '#', // attribute name
+				src: '../img/odintsovo-img-1.jpg' // attribute value
 			},
 			largeImage: {
-				alt: '',
-				url: ''
+				alt: '#',
+				src: '../img/odintsovo-img-2.jpg'
 			},
 			cardImage: {
-				alt: '',
-				url: ''
+				alt: '#',
+				src: '../img/odintsovo-img-3.jpg'
 			}
-		}
+		},
+		link: '#'
 	}
 ];
 
 const container = document.getElementById('room-slider');
 
 class RoomSlider {
-	constructor() {
-		//
+	constructor(container, dataToShow) {
+		this.props = {
+			data: [...dataToShow],
+			slideIndex: 0
+		}
+
+		this.renderSlide(container, this.props.slideIndex);
+		console.log(container);
+	}
+
+	renderSlide(container, idx) {
+		const {images, link} = this.props.data[idx];
+
+		container.innerText = '';
+
+		const tileBlock = this.createElement('div', 'projects__tile'),
+			infoBlock = this.createElement('div', 'projects__info'),
+			titleInfo = this.createElement('h4', 'projects__title-info'),
+			location = this.createElement('p', 'projects__location'),
+			square = this.createElement('span', 'projects__square'),
+			smallImage = this.createElement('img', 'projects__image_small', [images.smallImage]),
+			largeImage = this.createElement('img', 'projects__image_small', [images.largeImage]),
+			priceCard = this.createElement('div', 'projects__price-card'),
+			priceImage = this.createElement('img', 'projects__image_small', [images.cardImage]),
+			buttonlink = this.createElement('a', 'price-card__button', [{link}]);
+
+		infoBlock.append(titleInfo, location, square);
+		tileBlock.append(infoBlock, smallImage, largeImage);
+		priceCard.append(priceImage, buttonlink);
+		container.append(tileBlock, priceCard);
+	}
+
+	createElement(tagName, className, attributes) {
+		const element = document.createElement(tagName);
+		element.classList.add(className);
+
+		if (attributes) {
+			attributes.map(attribute => {
+				for (let key in attribute) {
+					element.setAttribute(key, attribute[key]);
+				}
+			});
+		}
+		return element;
 	}
 }
 
